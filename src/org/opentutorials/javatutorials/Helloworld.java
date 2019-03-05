c328e9212f5de3791f135ff242234445f5826ae1
Jun ho Lee <june20516@gmail.com>
2019-03-05 19:58:18 +0900

    init java tutorials & some examples

---
+++ src/org/opentutorials/javatutorials/Helloworld.java
@@ -0,0 +1,19 @@
+package org.opentutorials.javatutorials;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// data type : Int&double&float&string&char
+		System.out.println("Hello world");
+		System.out.println(2 + 3);
+		System.out.println(2.1 * 3);
+		System.out.println(3.5 / 7.1);
+		System.out.println("P");
+		System.out.println('P');
+		System.out.println("string");
+		System.out.println("\'string\'");
+		System.out.println("First\nSecond");
+		System.out.println("one"+" "+"and");
+	}
+
+}
---
+++ src/org/opentutorials/javatutorials/Variable.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int a = 5093;
+		System.out.println(a-3); //5090
+	}
+
+}
